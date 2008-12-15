27563c03a1d28fe3152a0073572e3bf08aa4f213
mike.c.jennings <mike.c.jennings@bb9bb3b3-0845-0410-b35b-8b76130e7852>
2008-12-15 16:51:36 +0000

    added getProtocol() getScheme() methods to HttpRequestImpl

--- trunk/minigse/java/com/google/opengse/HttpRequest.java
+++ trunk/minigse/java/com/google/opengse/HttpRequest.java
@@ -55,8 +55,28 @@ public interface HttpRequest {
 
   Enumeration<Locale> getLocales();
 
+  /**
+   * Returns the name and version of the protocol the request uses
+   * in the form <i>protocol/majorVersion.minorVersion</i>, for
+   * example, HTTP/1.1. For HTTP servlets, the value
+   * returned is the same as the value of the CGI variable
+   * <code>SERVER_PROTOCOL</code>.
+   *
+   * @return		a <code>String</code> containing the protocol
+   *			name and version number
+   */
   String getProtocol();
 
+  /**
+   * Returns the name of the scheme used to make this request,
+   * for example,
+   * <code>http</code>, <code>https</code>, or <code>ftp</code>.
+   * Different schemes have different rules for constructing URLs,
+   * as noted in RFC 1738.
+   *
+   * @return		a <code>String</code> containing the name
+   *			of the scheme used to make this request
+   */
   String getScheme();
 
   /**
