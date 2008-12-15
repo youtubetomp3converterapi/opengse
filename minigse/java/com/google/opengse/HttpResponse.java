1066c80ca812d12a82cf0662958cfa278c7f6dfc
mike.c.jennings <mike.c.jennings@bb9bb3b3-0845-0410-b35b-8b76130e7852>
2008-12-15 17:57:15 +0000

    Fixed more failing tests

--- minigse/java/com/google/opengse/HttpResponse.java
+++ minigse/java/com/google/opengse/HttpResponse.java
@@ -209,6 +209,42 @@ public interface HttpResponse {
    */
   void reset();
 
+  /**
+   * Sets the locale of the response, if the response has not been
+   * committed yet. It also sets the response's character encoding
+   * appropriately for the locale, if the character encoding has not
+   * been explicitly set using {@link #setContentType} or
+   * {@link #setCharacterEncoding}, <code>getWriter</code> hasn't
+   * been called yet, and the response hasn't been committed yet.
+   * If the deployment descriptor contains a
+   * <code>locale-encoding-mapping-list</code> element, and that
+   * element provides a mapping for the given locale, that mapping
+   * is used. Otherwise, the mapping from locale to character
+   * encoding is container dependent.
+   * <p>This method may be called repeatedly to change locale and
+   * character encoding. The method has no effect if called after the
+   * response has been committed. It does not set the response's
+   * character encoding if it is called after {@link #setContentType}
+   * has been called with a charset specification, after
+   * {@link #setCharacterEncoding} has been called, after
+   * <code>getWriter</code> has been called, or after the response
+   * has been committed.
+   * <p>Containers must communicate the locale and the character encoding
+   * used for the servlet response's writer to the client if the protocol
+   * provides a way for doing so. In the case of HTTP, the locale is
+   * communicated via the <code>Content-Language</code> header,
+   * the character encoding as part of the <code>Content-Type</code>
+   * header for text media types. Note that the character encoding
+   * cannot be communicated via HTTP headers if the servlet does not
+   * specify a content type; however, it is still used to encode text
+   * written via the servlet response's writer.
+   *
+   * @param loc  the locale of the response
+   *
+   * @see 		#getLocale
+   * @see 		#setContentType
+   * @see 		#setCharacterEncoding
+   */
   void setLocale(Locale loc);
 
   Locale getLocale();
