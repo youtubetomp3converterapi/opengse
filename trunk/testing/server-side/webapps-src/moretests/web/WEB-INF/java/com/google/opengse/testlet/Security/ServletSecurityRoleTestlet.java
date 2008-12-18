2bd5e7004f77830c8625598e7b86ca8e9841bf3e
wenbozhu <wenbozhu@bb9bb3b3-0845-0410-b35b-8b76130e7852>
2008-12-18 06:16:12 +0000

    Basic security tests.

--- trunk/testing/server-side/webapps-src/moretests/web/WEB-INF/java/com/google/opengse/testlet/Security/ServletSecurityRoleTestlet.java
+++ trunk/testing/server-side/webapps-src/moretests/web/WEB-INF/java/com/google/opengse/testlet/Security/ServletSecurityRoleTestlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
+ * In web.xml, security role may be specified with the servlet, i.e. the role
+ * required to access the servlet, via the "security-role-ref" element, which
+ * either links to a specific role with a different name, or if unspecified,
+ * a role with the same name.
+ *
  * @author Wenbo Zhu
  */
 public class ServletSecurityRoleTestlet extends HttpServlet {
@@ -16,7 +21,12 @@ public class ServletSecurityRoleTestlet extends HttpServlet {
   @Override
   protected void service(HttpServletRequest request,
       HttpServletResponse response) throws ServletException, IOException {
-    response.getWriter().println("security-role=" + request.getAuthType());
+    response.setContentType("text/plain");
+    String role = request.getParameter("target-role");
+    response.getWriter().println(role + "=" + request.isUserInRole(role));
+    // <role-name>role_name</role-name>  = the local role name
+    // <role-link>external_role</role-link> = the global role name
+    // and the API should always return "local role name".
   }
 }
 
