6d0e2a0a4914d6a26c55ed8a1a98eb7ca4a2c4ca
mike.c.jennings <mike.c.jennings@bb9bb3b3-0845-0410-b35b-8b76130e7852>
2009-02-08 18:18:04 +0000

    Updated ServletTestsWhichFailUnderTomcat

--- testing/client-side/java/com/google/opengse/clienttests/ServletTestsWhichFailUnderTomcat.java
+++ testing/client-side/java/com/google/opengse/clienttests/ServletTestsWhichFailUnderTomcat.java
@@ -27,100 +27,6 @@ import java.util.List;
 public class ServletTestsWhichFailUnderTomcat extends ServletTestsWhichConnectToARemoteServer {
 
   /**
-   * Test what request headers we have sent by invoking the servlet that
-   * responds to "*.hdr"
-   *
-   * Uses com.google.opengse.testlet.HttpSession.SessionInfoTestlet
-   *
-   * @throws Exception if anything goes wrong
-   */
-  @Test
-  public void testSessionCookie()
-      throws Exception {
-    HttpRequestAsserter get = createGetAssertion();
-    get.setUri("/contextpath/gimmecookie");
-//    get.setRequestHeader("X-my-special-header", "ExtraSpecial, VerySpecial");
-    get.setExpectedResponseLine("PASSED");
-    get.expectHeader("SET-COOKIE");
-    get.setExpectedResponseCode(200);
-    get.connectToServerAndAssert();
-    List<Cookie> cookies = get.getResponseCookies();
-    assertEquals("More than one cookie found", 1, cookies.size());
-    Cookie cookie = cookies.iterator().next();
-
-    assertNotNull("Invalid cookie", cookie.getPath());
-
-    // now we send that cookie back to the server
-    get = createGetAssertion("/contextpath/sessioninfo");
-//    get.expectHeader("SET-COOKIE");
-    get.setExpectedResponseCode(200);
-    get.setExpectedContentType("text/plain");
-    String originalSessionId = cookie.getValue();
-    String requestedId = originalSessionId + "foo";
-    cookie.setValue(requestedId);
-    get.addRequestCookie(cookie);
-    get.setProperty("requestedId", requestedId);
-    get.setExpectedResponseViaResource(
-        "com/google/opengse/golden/noSessionRequested.txt");
-    get.connectToServerAndAssert();
-  }
-
-  /**
-   * Tells the forwarding servlet to forward the request to
-   * {@code /bar/foo.test?woo=hoo} then checks the result.
-   *
-   * @throws Exception if anything goes wrong
-   */
-  @Test
-  public void testForward2()
-      throws Exception {
-    HttpRequestAsserter get = createGetAssertion();
-    get.setUri("/contextpath/fwd?to=/bar/foo.test%3Fwoo%3Dhoo");
-    get.setExpectedResponseLine(
-        "requestURI=/contextpath/bar/foo.test");
-    get.setExpectedResponseLine(
-        "contextPath=/contextpath");
-    get.setExpectedResponseLine(
-        "servletPath=/bar/foo.test");
-    get.setExpectedResponseLine(
-        "pathInfo=null");
-    /*
-     * we are forwarding to "/bar/foo.test?woo=hoo" from a servlet which gets
-     * a query string of "to=something" the servlet we forward to should then
-     * see a query string of "woo=hoo&to=something" (in this particular case,
-     * "something" is "/bar/foo.test%3Fwoo=hoo"
-     */
-//    get.setExpectedResponseLine(
-//        "queryString=woo=hoo&to=/bar/foo.test?woo=hoo");
-    get.setExpectedResponseLine(
-        "queryString=woo=hoo&to=/bar/foo.test%3Fwoo%3Dhoo");
-    get.setExpectedResponseLine(
-        "attribute javax.servlet.forward.context_path=/contextpath");
-    get.setExpectedResponseLine(
-        "attribute javax.servlet.forward.servlet_path=/fwd");
-    get.setExpectedResponseLine(
-        "attribute javax.servlet.forward.query_string=to=/bar/foo.test%3Fwoo%3Dhoo");
-    get.setExpectedResponseCode(200);
-    get.connectToServerAndAssert();
-  }
-
-  /**
-   * See XPoweredByHeaderTestlet for the expected header format.
-   *
-   * @throws Exception if anything goes wrong
-   */
-  @Test
-  public void testXPoweredByHeader()
-      throws Exception {
-    HttpRequestAsserter get = createGetAssertion();
-    get.setUri("/contextpath/XPoweredByHeaderTest");
-    get.expectHeader("X-Powered-By");
-//    get.setExpectedPassResponseLine();
-    get.setExpectedResponseCode(200);
-    get.connectToServerAndAssert();
-  }
-
-  /**
    * 500 is generated for an uncaught session exception.
    * Uses com.google.opengse.testlet.Error.UnhandledSessionExceptionTestlet
    *
@@ -136,39 +42,6 @@ public class ServletTestsWhichFailUnderTomcat extends ServletTestsWhichConnectTo
     get.connectToServerAndAssert();
   }
 
-  /**
-   * The same as the above TC, except the path comes without a trailing "/".
-   * According to the spec, this will be redirected (or rewritten) to
-   * the one with trailing "/", i.e. if there is not servlet URL matching.
-   *
-   * Note servlet URL matching has to be exact, unless it's a wildcard matching.
-   * @throws Exception if anything goes wrong
-   */
-  @Test
-  public void testPathWithoutTrailingSlashWelcomeFileAccess() throws Exception {
-    HttpRequestAsserter get = createGetAssertion("/contextpath/welcome");
-    // tomcat sends a redirect to the above url with a slash appended
-    // so we need to follow it
-    get.setFollowRedirects(true);
-    get.setExpectedResponseCode(200);
-    get.setExpectedResponseLine("welcome1");
-    get.connectToServerAndAssert();
-  }
-
-  /**
-   * There is only one welcome file under welcome/welcome, and we use the
-   * full path matching to locate the target - i.e. welcome2.txt.
-   * @throws Exception if anything goes wrong
-   */
-  @Test
-  public void testPathMatchingWelcomeFileAccess() throws Exception {
-    HttpRequestAsserter get = createGetAssertion();
-    get.setUri("/contextpath/welcome/welcome");
-    get.setExpectedResponseCode(200);
-    get.setExpectedResponseLine(
-        "welcome2");
-    get.connectToServerAndAssert();
-  }
 
   /**
    * Test a pure wildcard servlet mapping will return request paths correctly.
@@ -191,12 +64,11 @@ public class ServletTestsWhichFailUnderTomcat extends ServletTestsWhichConnectTo
   @Test
   public void testRequestPathsUnderWildcardOnlyServletMapping()
       throws Exception {
-    HttpRequestAsserter get = createGetAssertion();
-    get.setUri("/contextpath/servletpath2/pathinfo");
+    HttpRequestAsserter get = createGetAssertion("/contextpath/servletpath2/pathinfo");
     get.setExpectedContentType("text/plain");
     get.setExpectedResponseCode(200);
-    get.setExpectedResponseLine("servletPath=");      // =""
-    get.setExpectedResponseLine("pathInfo=/servletpath2/pathinfo");
+    get.setExpectedResponseLine("WildcardServletDispatchFilter.servletPath=");      // =""
+    get.setExpectedResponseLine("WildcardServletDispatchFilter.pathInfo=/servletpath2/pathinfo");
     get.connectToServerAndAssert();
   }
 
@@ -217,7 +89,7 @@ public class ServletTestsWhichFailUnderTomcat extends ServletTestsWhichConnectTo
   public void testErrorDispatcherFilter() throws Exception {
     HttpRequestAsserter get = createGetAssertion();
     get.setUri("/contextpath/Error599");
-    get.setExpectedResponseCode(200);
+    get.setExpectedResponseCode(599);
     get.setExpectedResponseLine(
         "Hello from ServletErrorFilter request");
     get.setExpectedResponseLine(
