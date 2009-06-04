de2c86c411351a7058dbd02a47465afcbd7d9e86
mike.c.jennings <mike.c.jennings@bb9bb3b3-0845-0410-b35b-8b76130e7852>
2009-06-04 06:41:31 +0000

    First stab at SSL support in OpenGSE

--- trunk/engines/minigse/java/com/google/opengse/ServletEngineConfigurationImpl.java
+++ trunk/engines/minigse/java/com/google/opengse/ServletEngineConfigurationImpl.java
@@ -16,7 +16,12 @@ package com.google.opengse;
 
 import com.google.opengse.util.PropertiesUtil;
 
+import javax.net.ssl.SSLServerSocketFactory;
 import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Iterator;
 
 /**
  * @author jennings
@@ -24,10 +29,16 @@ import java.util.Properties;
 public class ServletEngineConfigurationImpl
     implements ServletEngineConfiguration {
   public static final String KEY_PORT = "com.google.opengse.port";
+  public static final String KEY_SECURE = "secure";
   public static final String KEY_THREADS = "threads";
+  public static final String KEY_ENABLED_CIPHER_SUITES = "secure.enabled.cipher.suites";
 
   private int port;
   private int maxThreads;
+  private boolean secure;
+  private String[] enabledCipherSuites;
+  private static final String JAVAX_NET_SSL_KEYSTORE = "javax.net.ssl.keyStore";
+  private static final String JAVAX_NET_SSL_KEYSTORE_PASSWORD = "javax.net.ssl.keyStorePassword";
 
   public ServletEngineConfigurationImpl(Properties props) {
     final int noneSpecified = Integer.MAX_VALUE;
@@ -37,6 +48,81 @@ public class ServletEngineConfigurationImpl
           "No " + KEY_PORT + " property specified");
     }
     maxThreads = PropertiesUtil.getInteger(props, KEY_THREADS, 5);
+    maybeSetupSSL(props);
+  }
+
+  private void maybeSetupSSL(Properties props) {
+    secure = PropertiesUtil.getBoolean(props, KEY_SECURE, false);
+    if (!secure) {
+      return;
+    }
+
+    Set<String> supportedSuites = getSupportedCipherSuites();
+
+    String commaSuites = PropertiesUtil.getAliasedProperty(props, KEY_ENABLED_CIPHER_SUITES, null);
+    if (commaSuites == null) {
+      String commaSupportedSuites = toCommaDelimeteredString(supportedSuites);
+      throw new IllegalArgumentException("Cannot have " + KEY_SECURE
+          + "=true without specifying " + KEY_ENABLED_CIPHER_SUITES
+          + " something like: "
+          + KEY_ENABLED_CIPHER_SUITES + "=" + commaSupportedSuites + " would work");
+    }
+    StringTokenizer st = new StringTokenizer(commaSuites, ",");
+    String[] suites = new String[st.countTokens()];
+    int i = 0;
+    boolean allAnonymous = true;
+    while (st.hasMoreTokens()) {
+      suites[i] = st.nextToken();
+      if (!supportedSuites.contains(suites[i])) {
+        throw new IllegalArgumentException("Cipher suite '" + suites[i] + "' is not supported");
+      }
+      if (suites[i].indexOf("_anon_") == -1) {
+        allAnonymous = false;
+      }
+      ++i;
+    }
+    // now we check for mandatory system properties
+    if (!allAnonymous) {
+      // we only need a keystore if there are any non-anonymous cipher suites enabled
+      String keystore = System.getProperty(JAVAX_NET_SSL_KEYSTORE);
+      if (keystore == null) {
+        throw new IllegalArgumentException("No " + JAVAX_NET_SSL_KEYSTORE
+            + " system property specified."
+            + " This is needed unless only anonymous suites like 'SSL_DH_anon_WITH_RC4_128_MD5' are used");
+      }
+      String keystorepass = System.getProperty(JAVAX_NET_SSL_KEYSTORE_PASSWORD);
+      if (keystorepass == null) {
+        throw new IllegalArgumentException("No " + JAVAX_NET_SSL_KEYSTORE_PASSWORD + " system property specified");
+      }
+    }
+
+    enabledCipherSuites = suites;
+  }
+
+  public String[] getEnabledCipherSuites() {
+    return enabledCipherSuites;
+  }
+
+  private static Set<String> getSupportedCipherSuites() {
+    SSLServerSocketFactory factory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+    String[] supportedSuites = factory.getSupportedCipherSuites();
+    Set<String> suites = new TreeSet<String>();
+    for (String suite : supportedSuites) {
+      suites.add(suite);
+    }
+    return suites;
+  }
+
+  private static String toCommaDelimeteredString(Set<String> set) {
+    if (set.isEmpty()) {
+      return "";
+    }
+    Iterator<String> iter = set.iterator();
+    StringBuffer sb = new StringBuffer(iter.next());
+    while (iter.hasNext()) {
+      sb.append(',').append(iter.next());
+    }
+    return sb.toString();
   }
 
   public static ServletEngineConfiguration create(int port, int maxThreads) {
@@ -56,6 +142,10 @@ public class ServletEngineConfigurationImpl
 
   public String getServerType() {
     return "foo";
+  }
+
+  public boolean isSecure() {
+    return secure;
   }
 
   public boolean exitOnError() {
